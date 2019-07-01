56ff0521e53379118630a1748b75739a735d6a26
Martin Bektchiev <martin.bektchiev@progress.com>
2019-07-01 18:46:08 +0300

    fix(ios): CodeSign frameworks in final executable
    It is required for UIKit for Mac

--- lib/services/ios-project-service.ts
+++ lib/services/ios-project-service.ts
@@ -268,6 +268,7 @@ export class IOSProjectService extends projectServiceBaseLib.PlatformProjectServ
 			const frameworkAddOptions: IXcode.Options = { customFramework: true };
 			if (this.isDynamicFramework(frameworkPath)) {
 				frameworkAddOptions["embed"] = true;
+				frameworkAddOptions["sign"] = true;
 			}
 
 			const frameworkRelativePath = '$(SRCROOT)/' + this.getLibSubpathRelativeToProjectPath(frameworkPath, projectData);
